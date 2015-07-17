2506b77d5b764917c37e608b2437ed4e542a9948
andezhou <zhoujunfeng007@126.com>
2015-07-17 10:46:38 +0800

    Initial Commit

---
+++ TLCollectionViewCircleLayout－demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TLCollectionViewCircleLayout－demo
+//
+//  Created by andezhou on 15/7/17.
+//  Copyright (c) 2015年 andezhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TLCollectionViewCircleLayout－demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TLCollectionViewCircleLayout－demo
+//
+//  Created by andezhou on 15/7/17.
+//  Copyright (c) 2015年 andezhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
